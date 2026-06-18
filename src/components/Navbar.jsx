8523b8683f51769f22e75ec0e6fb345fe8ed4810
VaibhaV <vaibhav.codes09@gmail.com>
2026-06-18 20:34:06 +0530

    Fixed navbar on mobile screens, now displaying full list

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,8 +7,8 @@ import LogoIcon from "../assets/logo.png";
 const Navbar = () => {
   const [hamburgerOpen, setHamburgerOpen] = useState(false);
   return (
-    <nav className="liquid-glass mt-4 h-16 rounded-lg w-[90%] sm:w-[80%] md:w-[50%] flex justify-around mx-auto fixed top-0 left-0 right-0 z-10">
-      <img src={LogoIcon} alt="Logo" className="scale-200 md:pl-1" />
+    <nav className="liquid-glass mt-4 rounded-lg w-[90%] sm:w-[80%] md:w-[50%] flex justify-around items-center mx-auto fixed top-0 left-0 right-0 z-10 transition-all-ease-in-out duration-200">
+      <img src={LogoIcon} alt="Logo" className="scale-400 md:scale-250  h-16  md:pl-1" />
       <ul className="hidden md:flex px-6 py-4 gap-9 justify-around items-center ">
         <li>
           <a href="#home">Home</a>
